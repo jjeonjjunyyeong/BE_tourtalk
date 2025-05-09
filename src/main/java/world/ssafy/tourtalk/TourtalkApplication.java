ad27d6fcda43583f66e6dc00a8674071c7b305ea
전준영 <wjswnsdud354@gmail.com>
2025-05-09 16:30:36 +0900

    init

---
+++ src/main/java/world/ssafy/tourtalk/TourtalkApplication.java
@@ -0,0 +1,13 @@
+package world.ssafy.tourtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourtalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourtalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/world/ssafy/tourtalk/TourtalkApplicationTests.java
@@ -0,0 +1,13 @@
+package world.ssafy.tourtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourtalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
